efed6c4bfe8753aaa4b3ddb0a0f14eb8e99ed8e7
Delfos <denisfag_lab@yahoo.com.br>
2026-07-10 09:39:46 +0200

    gereal

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
+
+
+/*Add new photo */
+let elemento = document.querySelector("#IdCarImg");
+let IdLink =  localStorage.getItem("IdLink")
+elemento.src = "compras/"+IdLink+".jpg";
+
+
+/*Save idCar on localStorege*/
 function SalveIdCar(IdCar){
 event.preventDefault();
 localStorage.setItem("IdLink", IdCar.id);
 window.location.href = IdCar.href;
-}
-function wichCar(){
-let elemento = document.querySelector("#IdCarImg");
-let IdLink =  localStorage.getItem("IdLink")
-elemento.src = "compras/"+IdLink+".jpg";
 }
